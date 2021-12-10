d694432fe79ff81c952b521b87aa3de2b2c998b5
Wong Jie Peng <76239879+jiepengwong@users.noreply.github.com>
2021-12-10 17:36:57 +0800

    some minor changes and tweaking

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@
 require("dotenv").config();
 
 const express = require("express");
-const app = express;
+const app = express();
 
 // Calling like an API to the express backend server, convert into json
 app.use(express.json());
@@ -12,11 +12,13 @@ app.use(express.json());
 //set up stripe
 
 // this would be the API access key
-const stripe = require("stripe"(process.env.STRIPE_PRIVATE_KEY));
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 
-// Simple data base here
+// Simple data base here, there is a huge secruity list here because we dont want user to edit the price
 const storeItems = new Map([
   [1, { priceInCents: 10000, name: "Fintech Course" }],
   [2, { priceInCents: 20000, name: "Learn CSS" }],
 ]);
+
+// This is the port number here
 app.listen(3000);
